d9a94099bd16b93aa014fb3e45dd8d20fe6d7c5b
OOP_CS_2022_038 <bandara-cs22038@stu.kln.ac.lk>
2025-09-12 17:45:14 +0530

    Initial commit

--- src/LW_01/Q4.java
+++ src/LW_01/Q4.java
@@ -1,5 +1,19 @@
 package LW_01;
 
+import javax.swing.*;
+
 public class Q4 {
 
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame();
+        window.setTitle("My First Frame");
+        window.setVisible(true);
+        window.setSize(300,200);
+        window.setLocation(100,50);
+
+        //window.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        //window.setResizable(true);
+
+    }
 }
