54f081a4139e7921a8a60517cf31e4d4c5786250
tharupathii <tharupathi19@gmail.com>
2025-11-17 10:32:14 +0530

    Initial commit

---
+++ src/LW_02/Q_01.java
@@ -0,0 +1,12 @@
+package LW_02;
+
+public class Q_01 {
+    public static void main(String[] args) {
+        for (int i = 10; i < 50; i++) {
+            System.out.print(i+" ");
+            if(i%10 == 9){
+                System.out.println("");
+            }
+        }
+    }
+}
